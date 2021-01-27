400c3b6c68c245dae253c73020fcf4eec1e1c2d3
Deonatan Dinata <69316014+Deonatan@users.noreply.github.com>
2021-01-27 16:44:43 +0800

    Add files via upload

---
+++ PlotData.m
@@ -0,0 +1,7 @@
+%Plot Data Function 
+function plotData (x, y) 
+  figure; 
+  plot(x, y ,'rx' , 'MarkerSize' , 10); 
+  xlabel('Profit in $10,000s'); 
+  ylabel('Population of City in 10,000'); 
+  end 
